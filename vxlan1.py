2e5ed506e0c5e04865d237bc025527139ce56faf
nhok8t1 <dohoangsonnd@gmail.com>
2022-07-21 16:05:50 +0700

    v1

---
+++ vxlan1.py
@@ -0,0 +1,29 @@
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSKernelSwitch, UserSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.link import Link, TCLink
+
+
+def emptyNet():
+    net = Mininet()
+    info('*** Adding Controller\n')
+    info('*** Adding Host\n')
+    red1 = net.addHost('red1', ip='10.0.10.10', mac='00:00:00:00:00:01')
+    blue1 = net.addHost('red1', ip='10.0.10.10', mac='00:00:00:00:00:01')
+    info('*** Adding Switch\n')
+    s1 = net.addSwitch('s1')
+    info('*** Creating Link\n')
+    net.addLink(red1, s1)
+    net.addLink(blue1, s1)
+    info('*** Starting Network\n')
+    net.start()
+    info('*** Running CLI\n')
+    CLI(net)
+    info('*** Stopping Network\n')
+    net.stop()
+
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    emptyNet()
