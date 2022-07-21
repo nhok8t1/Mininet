3e4a8c039c4258b0f8aeca4c0264dcce8a15a7ac
nhok8t1 <dohoangsonnd@gmail.com>
2022-07-22 00:27:11 +0700

    v7

--- vxlan1.py
+++ vxlan1.py
@@ -6,18 +6,21 @@ from mininet.link import Link, TCLink
 
 
 def emptyNet():
-    net = Mininet()
     info('*** Adding Controller\n')
+    net = Mininet(controller=RemoteController, link=TCLink, switch=OVSKernelSwitch)
     info('*** Adding Host\n')
     red1 = net.addHost('red1', ip='10.0.10.10', mac='00:00:00:00:00:01')
     blue1 = net.addHost('blue1', ip='10.0.10.10', mac='00:00:00:00:00:01')
     info('*** Adding Switch\n')
     s11 = net.addSwitch('s11')
+    c0 = net.addController('c0', controller=RemoteController, ip='127.0.0.1', port=6633)
     info('*** Creating Link\n')
     net.addLink(red1, s11)
     net.addLink(blue1, s11)
     info('*** Starting Network\n')
-    net.start()
+    net.build()
+    c0.start()
+    s11.start([c0])
     info('*** Running CLI\n')
     CLI(net)
     info('*** Stopping Network\n')
--- vxlan2.py
+++ vxlan2.py
@@ -6,18 +6,21 @@ from mininet.link import Link, TCLink
 
 
 def emptyNet():
-    net = Mininet()
     info('*** Adding Controller\n')
+    net = Mininet(controller=RemoteController, link=TCLink, switch=OVSKernelSwitch)
     info('*** Adding Host\n')
     red2 = net.addHost('red2', ip='10.0.10.20', mac='00:00:00:00:00:02')
     blue2 = net.addHost('blue2', ip='10.0.10.20', mac='00:00:00:00:00:02')
     info('*** Adding Switch\n')
     s22 = net.addSwitch('s22')
+    c0 = net.addController('c0', controller=RemoteController, ip='127.0.0.1', port=6633)
     info('*** Creating Link\n')
     net.addLink(red2, s22)
     net.addLink(blue2, s22)
     info('*** Starting Network\n')
-    net.start()
+    net.build()
+    c0.start()
+    s22.start([c0])
     info('*** Running CLI\n')
     CLI(net)
     info('*** Stopping Network\n')
